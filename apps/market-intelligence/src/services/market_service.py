54496d701563eba4a47e09c45d8d2e912a140137
hkan84 <hakanboztepe@gmail.com>
2026-08-05 12:19:39 +0300

    Add get_market method to MarketService class

--- apps/market-intelligence/src/services/market_service.py
+++ apps/market-intelligence/src/services/market_service.py
@@ -4,3 +4,6 @@ from src.providers.market import MarketProvider
 class MarketService:
     def __init__(self):
         self.provider = MarketProvider()
+
+def get_market(self):
+    return self.provider.get_market()
